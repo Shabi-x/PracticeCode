bb948989d77c1f0e8575299f7445a7ac1c70fbd1
shabix <2936670268@qq.com>
2025-02-05 19:53:45 +0800

    feat: 手写curry化记忆

--- curry.js
+++ curry.js
@@ -1,22 +1,36 @@
+/**
+ * 核心思路
+ * 参数达到length时，直接执行fn
+ * 未达到length时，返回一个新函数，新函数的参数为之前的参数和新的参数
+ */
+/**
+ *
+ * @param {*} fn
+ * apply方法被用来调用fn函数，并将arg1数组中的所有元素作为参数传递给fn。
+ * 这是因为curry函数的目的是将一个多参数的函数转换为一系列单参数的函数
+ *而apply方法允许你将一个数组作为参数传递给函数，这在处理不定数量的参数时非常有用。
+ */
 // function curry(fn) {
-//   return function curried(args) {
-//     if (args.length >= fn.length) {
-//       return fn.apply(this, args);
+//   return function curried(...arg1) {
+//     if (arg1.length >= fn.length) {
+//       return fn.apply(this, arg1);
 //     } else {
-//       return function (...part) {
-//         return curried.apply(this, args.concat(part));
+//       return function (...arg2) {
+//         return curried.apply(this, arg1.concat(arg2));
 //       };
 //     }
 //   };
 // }
-function curry(fn) {
-  return function curried(args) {
-    if (args.length >= fn) {
-      return fn.apply(this, args);
-    } else {
-      return function (...part) {
-        return curried.apply(this, args.concat(part));
-      };
+
+
+function curry(fn){
+  return function curried(...args1){
+    if(args1.length>=fn.length){
+      return fn.apply(this,args1)
+    }else{
+      return function(...args2){
+        return curried.apply(this.args1.concat(args2))
+      }
     }
-  };
+  }
 }
