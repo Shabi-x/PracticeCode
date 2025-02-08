ae218118f68e060a22960860ae474cce23c89226
dundun003 <liangshaojun@papegames.net>
2025-02-08 16:45:19 +0800

    feat: 手写发布订阅

--- subscribe&public.js
+++ subscribe&public.js
@@ -5,9 +5,10 @@ class EventEmitter {
 
   //如果该事件类型已经存在对应的回调函数数组，则将回调函数追加到数组中
   //否则创建一个新的数组并将回调函数存入。
-  on(type, callback) {
+  //订阅
+  subscribe(type, callback) {
     if (!this.event[type]) {
-      this.event[type] = [callback];
+      this.event[type] = [];
     } else {
       this.event[type].push(callback);
     }
@@ -15,13 +16,37 @@ class EventEmitter {
 
   //用于触发事件，接受一个参数列表 args。
   //如果存在对应的事件类型，就依次执行该类型下的所有回调函数，并将参数传递给它们。
-  emit(type, ...args) {
+  publish(type, ...args) {
     if (!this.event[type]) {
       return;
     } else {
       this.event[type].forEach((callback) => {
-        callback(...args);
+        callback.apply(this, args);
       });
+    }
+  }
+}
+
+
+//PRACTICE
+class publishAndSubscribe {
+  constructor() {
+    this.event = {};
+  }
+  publish(eventName,...args){
+    if(!this.event[eventName]){
+      return 
+    }else{
+      this.event[eventName].forEach((callback)=>{
+        callback.apply(this,args)
+      })
+    }
+  }
+  subscribe(eventName,callback){
+    if(!this.event[eventName]){
+      this.event[eventName]=[]
+    }else{
+      this.event[eventName].push(callback)
     }
   }
 }
