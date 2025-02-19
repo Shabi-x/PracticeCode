5ccf9afb03f1b52305d365f5b1f974dee30a9a27
shabix <2936670268@qq.com>
2025-02-19 14:22:48 +0800

    feat: 公共祖先

--- LRU.js
+++ LRU.js
@@ -41,10 +41,55 @@
 //
 //
 //
+// class LRU {
+//   constructor(capacity) {
+//     this.capacity = capacity;
+//     this.map = new Map();
+//   }
+
+//   get(key) {
+//     if (this.map.has(key)) {
+//       const value = this.map.get(key);
+//       this.map.delete(key);
+//       this.map.set(key, value);
+//       return value;
+//     } else {
+//       return -1;
+//     }
+//   }
+
+//   set(key, value) {
+//     if (this.map.has(key)) {
+//       this.map.delete(key);
+//       this.map.set(key, value);
+//     } else {
+//       if (this.capacity < this.map.size) {
+//         this.map.delete(this.map.keys().next().value);
+//         this.map.set(key, value);
+//       } else {
+//         this.map.set(key, value);
+//       }
+//     }
+//   }
+// }
+
 class LRU {
   constructor(capacity) {
     this.capacity = capacity;
     this.map = new Map();
+  }
+  set(key, value) {
+    if (this.map.has(key)) {
+      this.map.delete(key);
+      this.map.set(key, value);
+    }else{
+      if(this.map.size>this.capacity){
+        this.map.delete(this.map.keys().next().value)
+        this.map.set(key,value)
+      }else{
+        this.map.set(key,value)
+      }
+    }
   }
 
   get(key) {
@@ -55,20 +100,6 @@ class LRU {
       return value;
     } else {
       return -1;
-    }
-  }
-
-  set(key, value) {
-    if (this.map.has(key)) {
-      this.map.delete(key);
-      this.map.set(key, value);
-    } else {
-      if (this.capacity < this.map.size) {
-        this.map.delete(this.map.keys().next().value);
-        this.map.set(key, value);
-      } else {
-        this.map.set(key, value);
-      }
     }
   }
 }
