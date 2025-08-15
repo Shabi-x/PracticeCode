9688c99d56d7fe88b685d3012fa1647a0cbb1698
shabix <2936670268@qq.com>
2025-08-15 10:11:49 +0800

    update

--- subscribe&public.js
+++ subscribe&public.js
@@ -1,47 +1,16 @@
-class EventEmitter {
-  constructor() {
-    this.event = {}; // 'run': [fun]
-  }
-
-  //如果该事件类型已经存在对应的回调函数数组，则将回调函数追加到数组中
-  //否则创建一个新的数组并将回调函数存入。
-  //订阅
-  subscribe(type, callback) {
-    if (!this.event[type]) {
-      this.event[type] = [];
-    } else {
-      this.event[type].push(callback);
-    }
-  }
-
-  //用于触发事件，接受一个参数列表 args。
-  //如果存在对应的事件类型，就依次执行该类型下的所有回调函数，并将参数传递给它们。
-  publish(type, ...args) {
-    if (!this.event[type]) {
-      return;
-    } else {
-      this.event[type].forEach((callback) => {
-        callback.apply(this, args);
-      });
-    }
-  }
-}
-
 class EventBus {
   constructor() {
     this.event = {};
-    // 'run' : []
   }
-
-  subscribe(type, callback) {
+  // 订阅事件
+  on(type, callback) {
     if (!this.event[type]) {
       this.event[type] = [];
-    } else {
-      this.event[type].push(callback);
     }
+    this.event[type].push(callback);
   }
-
-  publish(type, ...args) {
+  // 触发事件
+  emit(type, ...args) {
     if (!this.event[type]) {
       return;
     } else {
@@ -51,3 +20,12 @@ class EventBus {
     }
   }
 }
+
+
+// 订阅事件
+bus.on('onchange', (data) => {
+  console.log('数据变化了:', data);
+});
+
+// 触发事件  
+bus.emit('onchange', { value: 'new data' });
