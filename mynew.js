145fcd9f7140725a7d73bf4feaab8e4c07c42488
shabix <2936670268@qq.com>
2025-08-18 16:34:18 +0800

    update

--- mynew.js
+++ mynew.js
@@ -11,8 +11,18 @@
 // console.log(myNew.prototype.constructor === myNew);
 
 function new1(constructor, ...args) {
-  let obj = {};
+  let obj = new Object();
   obj.__proto__ = constructor.prototype;
   const result = constructor.apply(obj, args);
   return result instanceof Object ? result : obj; //因为不确定构造函数返回什么类型，所以要判断一下
 }
+
+
+function create(fn, ...args) {
+  var obj = Object.create(fn.prototype);
+
+  // 3、执行fn，并将obj作为内部this。使用 apply，改变构造函数 this 的指向到新建的对象，这样 obj 就可以访问到构造函数中的属性
+  var res = fn.apply(obj, args);
+  // 4、如果fn有返回值，则将其作为new操作返回内容，否则返回obj
+  return res instanceof Object ? res : obj;
+};
