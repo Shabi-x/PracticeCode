8a59fedc6673c94820f33ada39793c007e99541d
shabix <2936670268@qq.com>
2025-09-03 15:41:20 +0800

    update

--- leetcode/46.全排列.js
+++ leetcode/46.全排列.js
@@ -9,21 +9,16 @@
  */
 const permute = function (nums) {
     const result = [];
-    const used = new Array(nums.length).fill(false); // 用于记录哪些元素已经被使用
-
-    function dfs(combine) {
-        if (combine.length === nums.length) {
-            result.push([...combine]); // 将当前排列加入结果集
+    function dfs(path) {
+        if (path.length === nums.length) {
+            result.push([...path]); // 如果当前path已经达到目标长度，则将其添加到结果中
             return;
         }
         for (let i = 0; i < nums.length; i++) {
-            if (!used[i]) {
-                combine.push(nums[i]); // 将当前元素加入排列
-                used[i] = true; // 标记该元素已被使用
-                dfs(combine); // 递归处理下一个元素
-                used[i] = false; // 回溯，取消标记
-                combine.pop(); // 回溯，移除当前元素
-            }
+            if(path.includes(nums[i])) continue; // 如果当前节点已被使用，则跳过
+            path.push(nums[i]); // 将当前节点推入
+            dfs(path); // 已经推入了一个点、两个点的时候，继续DFS
+            path.pop(); // 回溯，例如已经推进去三个了，就回退一次去考虑其他可能
         }
     }
 
